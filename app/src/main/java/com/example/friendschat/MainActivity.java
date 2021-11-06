3d967a3efc07f412a9c76caf0352e51aef95208b
宇轩 王 <1465342176@qq.com>
2021-11-06 21:49:56 +0800

    complete RegisterPage

--- app/src/main/java/com/example/friendschat/MainActivity.java
+++ app/src/main/java/com/example/friendschat/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.friendschat;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -10,5 +11,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Intent intent = getIntent();
+        String email = intent.getStringExtra("email");
+        String username = intent.getStringExtra("username");
+        String password = intent.getStringExtra("password");
+
+
     }
 }
--- app/src/main/java/com/example/friendschat/RegisterPage.java
+++ app/src/main/java/com/example/friendschat/RegisterPage.java
@@ -2,15 +2,51 @@ package com.example.friendschat;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class RegisterPage extends AppCompatActivity {
 
-
+    EditText editTextEmail;
+    EditText editTextUsername;
+    EditText editTextPassword;
+    Button buttonRegister;
+    TextView warningMsg;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register_page);
+
+        editTextEmail = findViewById(R.id.editText_Email);
+        editTextUsername = findViewById(R.id.editText_Username);
+        editTextPassword = findViewById(R.id.editText_Password);
+        buttonRegister = findViewById(R.id.button_register);
+        warningMsg = findViewById(R.id.textView_warringMessage);
+
+
+    }
+
+    public void  setRegisterBtnOnClick(View view){
+
+        String email = editTextEmail.getText().toString();
+        String username = editTextEmail.getText().toString();
+        String password = editTextPassword.getText().toString();
+
+        if (email.isEmpty() || username.isEmpty() || password.isEmpty()){
+            warningMsg.setText(getString(R.string.warning));
+        }else {
+            Intent intent = new Intent(this,MainActivity.class);
+            intent.putExtra("email",email);
+            intent.putExtra("username",username);
+            intent.putExtra("password",password);
+            startActivity(intent);
+        }
+
+
     }
 }
