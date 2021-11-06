23e955f64c152c283acceff5290f9ef71e06020d
宇轩 王 <1465342176@qq.com>
2021-11-06 21:02:11 +0800

    finished RegisterPage view

---
+++ app/src/main/java/com/example/friendschat/RegisterPage.java
@@ -0,0 +1,16 @@
+package com.example.friendschat;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RegisterPage extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register_page);
+    }
+}
